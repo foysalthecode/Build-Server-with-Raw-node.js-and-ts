df2bed8b38ae7c3f01363caa9dea161dfd976b55
foysalthecode <ahmedmdfoysal123@gmail.com>
2025-11-25 09:15:13 +0600

    created post api with raw node.js and install ts-node-dev

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import http, { IncomingMessage, Server, ServerResponse } from "http";
 import path from "path";
+import config from "./config";
 
 const server: Server = http.createServer(
   (req: IncomingMessage, res: ServerResponse) => {
     console.log("Server is running....");
 
+    //Root route
     if (req.url === "/" && req.method === "GET") {
       res.writeHead(200, { "content-type": "application/json" });
       res.end(
@@ -14,9 +16,45 @@ const server: Server = http.createServer(
         })
       );
     }
+
+    //health route
+    if (req.url == "/api" && req.method === "GET") {
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(
+        JSON.stringify({
+          message: "Health status ok",
+          path: req.url,
+        })
+      );
+    }
+
+    //post api
+    if (req.url === "/api/users" && req.method === "POST") {
+      // const user = {
+      //   id: 1,
+      //   name: "alice",
+      // };
+      // res.writeHead(200, { "content-type": "application/json" });
+      // res.end(JSON.stringify(user));
+
+      let body = "";
+      req.on("data", (chunk) => {
+        body += chunk.toString();
+      });
+      req.on("end", () => {
+        try {
+          const parseBody = JSON.parse(body);
+          console.log(parseBody);
+          console.log("console current changes");
+          res.end(JSON.stringify(parseBody));
+        } catch (err: any) {
+          console.log(err?.message);
+        }
+      });
+    }
   }
 );
 
-server.listen(5000, () => {
-  console.log(`Server is running on port ${5000}`);
+server.listen(config.port, () => {
+  console.log(`Server is running on port ${config.port}`);
 });
