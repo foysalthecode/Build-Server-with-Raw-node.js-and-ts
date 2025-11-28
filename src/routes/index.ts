2cc89925bc7fd24de268fd3af468556327ecfc64
foysalthecode <ahmedmdfoysal123@gmail.com>
2025-11-28 23:43:56 +0600

    solved the port error

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,7 +7,6 @@ addRoutes("GET", "/", (req, res) => {
   sendJson(res, 200, { message: "Hello from node js and ts", path: req.url });
 });
 
-
 //get route
 addRoutes("GET", "/api", (req, res) => {
   sendJson(res, 200, {
@@ -15,7 +14,6 @@ addRoutes("GET", "/api", (req, res) => {
     path: req.url,
   });
 });
-
 
 //post route
 addRoutes("POST", "/api/users", async (req, res) => {
@@ -32,4 +30,33 @@ addRoutes("POST", "/api/users", async (req, res) => {
   sendJson(res, 201, { success: true, data: body });
 });
 
+//put route
 
+addRoutes("PUT", "/api/users/:id", async (req, res) => {
+  const { id } = (req as any).params;
+  const body = await parseBody(req);
+
+  const users = readUsers();
+
+  const index = users.findIndex((user: any) => user.id == id);
+
+  if (index === -1) {
+    sendJson(res, 404, {
+      success: false,
+      message: "User not found",
+    });
+  }
+
+  users[index] = {
+    ...users[index],
+    ...body,
+  };
+
+  writeUsers(users);
+
+  sendJson(res, 202, {
+    success: true,
+    message: `id ${id} user updated`,
+    data: users[index],
+  });
+});
--- src/server.ts
+++ src/server.ts
@@ -2,33 +2,7 @@ import http, { IncomingMessage, Server, ServerResponse } from "http";
 import config from "./config";
 import { RouteHandler, routes } from "./helpers/RouteHandler";
 import "./routes";
-
-function findDynamicRoutes(method: string, url: string) {
-  const methodMap = routes.get(method);
-  if (!methodMap) return null;
-
-  for (const [routePath, handler] of methodMap.entries()) {
-    const routesParts = routePath.split("/");
-    const urlParts = url.split("/");
-
-    if (routesParts.length !== urlParts.length) continue;
-    const params: any = {};
-    let matched = true;
-
-    for (let i = 0; i < routesParts.length; i++) {
-      if (routesParts[i]?.startsWith(":")) {
-        params[routesParts[i]?.substring(1)!] = urlParts[i];
-      } else if (routesParts[i] !== urlParts[i]) {
-        matched = false;
-        break;
-      }
-    }
-    if (matched) {
-      return { handler, params };
-    }
-  }
-  return null;
-}
+import findDynamicRoutes from "./helpers/dynamicRoutesHandler";
 
 const server: Server = http.createServer(
   (req: IncomingMessage, res: ServerResponse) => {
@@ -59,5 +33,6 @@ const server: Server = http.createServer(
 );
 
 server.listen(config.port, () => {
+  console.log(`Server is running on port ${config.port}`);
   console.log(`Server is running on port ${config.port}`);
 });
